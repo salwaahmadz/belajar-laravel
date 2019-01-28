e6f693608d4f21f729875186fcb2b479a9700a53
salwaahmadz <salwaahmadz888@gmail.com>
2019-01-28 17:00:46 +0700

    Penambahan Gambar Baru dan Keterangan

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -78,12 +78,12 @@
         <!-- Featured Project Row -->
         <div class="row align-items-center no-gutters mb-4 mb-lg-5">
           <div class="col-xl-8 col-lg-7">
-            <img class="img-fluid mb-3 mb-lg-0" src="asset/img/bg-masthead.jpg" alt="">
+            <img class="img-fluid mb-3 mb-lg-0" src="asset/img/Blue Eye Fantasy Cat.jpg" alt="">
           </div>
           <div class="col-xl-4 col-lg-5">
             <div class="featured-text text-center text-lg-left">
-              <h4>Shoreline</h4>
-              <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+              <h4>Blue Eyes Cat Fantasy</h4>
+              <p class="text-black-50 mb-0">Blue Eyes Cat Fantasy is one of my favorite picture. Combined from some photo to become a great picture.</p>
             </div>
           </div>
         </div>
@@ -91,14 +91,14 @@
         <!-- Project One Row -->
         <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
           <div class="col-lg-6">
-            <img class="img-fluid" src="asset/img/demo-image-01.jpg" alt="">
+            <img class="img-fluid" src="asset/img/Bridge & Beautiful Sky.jpg" alt="">
           </div>
           <div class="col-lg-6">
             <div class="bg-black text-center h-100 project">
               <div class="d-flex h-100">
                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                  <h4 class="text-white">Misty</h4>
-                  <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                  <h4 class="text-white">Bridge & Beautiful Sky</h4>
+                  <p class="mb-0 text-white-50">a man sits on a bridge combined with a beautiful sky.</p>
                   <hr class="d-none d-lg-block mb-0 ml-0">
                 </div>
               </div>
@@ -109,14 +109,14 @@
         <!-- Project Two Row -->
         <div class="row justify-content-center no-gutters">
           <div class="col-lg-6">
-            <img class="img-fluid" src="asset/img/demo-image-02.jpg" alt="">
+            <img class="img-fluid" src="asset/img/Sunset.jpg" alt="">
           </div>
           <div class="col-lg-6 order-lg-first">
             <div class="bg-black text-center h-100 project">
               <div class="d-flex h-100">
                 <div class="project-text w-100 my-auto text-center text-lg-right">
-                  <h4 class="text-white">Mountains</h4>
-                  <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                  <h4 class="text-white">Sunset Phenomenon</h4>
+                  <p class="mb-0 text-white-50">a combined photo of the sunset and a man standing on the edge of the lake.</p>
                   <hr class="d-none d-lg-block mb-0 mr-0">
                 </div>
               </div>
