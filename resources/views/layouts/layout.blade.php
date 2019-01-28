7225f2e74da29a023ec840640a811b4f08ac6a56
salwaahmadz <salwaahmadz888@gmail.com>
2019-01-28 16:08:35 +0700

    Hapus Beberapa Function

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -41,9 +41,6 @@
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#projects">Portofolio</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
-            </li>
           </ul>
         </div>
       </div>
@@ -53,8 +50,8 @@
     <header class="masthead">
       <div class="container d-flex h-100 align-items-center">
         <div class="mx-auto text-center">
-          <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
-          <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
+          <h1 class="mx-auto my-0 text-uppercase">Welcome</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5"> </h2>
           <a href="#about" class="btn btn-primary js-scroll-trigger">Get Started</a>
         </div>
       </div>
@@ -131,23 +128,6 @@
     </section>
 
     <!-- Signup Section -->
-    <section id="signup" class="signup-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-10 col-lg-8 mx-auto text-center">
-
-            <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-            <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
-
-            <form class="form-inline d-flex">
-              <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address...">
-              <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
-            </form>
-
-          </div>
-        </div>
-      </div>
-    </section>
 
     <!-- Contact Section -->
     <section class="contact-section bg-black">
